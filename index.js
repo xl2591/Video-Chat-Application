10d7591a756a55051ce439455e74bf2fae0f4c61
Xiaojingliu <xl2591@york.ac.uk>
2021-02-28 17:40:15 +0000

    refresh

--- index.js
+++ index.js
@@ -16,6 +16,40 @@ let server = app.listen(4000, function () {
     console.log("User Connected :" + socket.id);
 
     socket.on("join",function(roomName){
+
+      var rooms = io.sockets.adapter.rooms;
+      var room = rooms.get(roomName);
+      console.log(rooms);
       
+      if(room == undefined){
+        socket.join(roomName);
+        console.log("Room Created");
+        socket.emit("created")
+      } else if (room.size == 1){
+        socket.join(roomName);
+        console.log("Room Joined");
+        socket.emit("joined")
+      } 
+      else {
+        
+        socket.emit("full")
+      }
+      console.log(rooms);
     })
-  })
+    socket.on("ready",function(ready,roomName){
+
+      socket.broadcast.to(roomName).emit("ready");
+    });
+    socket.on("candidate",function(candidate,roomName){
+
+      socket.broadcast.to(roomName).emit("candidate",candidate);
+    });
+    socket.on("offer",function(offer,roomName){
+
+      socket.broadcast.to(roomName).emit("offer".offer);
+    });
+    socket.on("answer",function(answer,roomName){
+
+      socket.broadcast.to(roomName).emit("answer",answer);
+    });
+  });
--- public/chat.js
+++ public/chat.js
@@ -5,6 +5,8 @@ var joinButton = document.getElementById("join");
 var userVideo = document.getElementById("user-video");
 var peerVideo = document.getElementById("peer-video");
 var roomInput = document.getElementById("roomName");
+var roomName ;
+var creator = false;
 
 joinButton.addEventListener('click',function() {
 
@@ -12,24 +14,59 @@ joinButton.addEventListener('click',function() {
         alert("Please enter a room name")
     }
     else{
-        socket.emit("join",roomInput.value)
-        navigator.mediaDevices
-        .getUserMedia(
-            { audio: true, 
-            video: { width: 1280, height: 720 }, 
-        })
-        .then(function (stream) {
-        userStream = stream;
-        divVideoChatLobby.style = "display:none";
-        userVideo.srcObject = stream;
-        userVideo.onloadedmetadata = function(e) {
-            userVideo.play();
-        };
-    
-    })
-        .catch(function(err){
-            alert("Counld't access user media");
-        }
-        );
+        socket.emit("join",roomName);
     }
 });
+
+socket.on("created",function(){
+    creator = true;
+  
+    navigator.mediaDevices
+    .getUserMedia(
+        { audio: false, 
+        video: { width: 1280, height: 720 }, 
+    })
+    .then(function (stream) {
+    userStream = stream;
+    divVideoChatLobby.style = "display:none";
+    userVideo.srcObject = stream;
+    userVideo.onloadedmetadata = function(e) {
+        userVideo.play();
+    };
+
+})
+    .catch(function(err){
+        alert("Counld't access user media");
+    }
+    );
+
+});
+socket.on("joined",function(){
+    creator = false;
+   
+    navigator.mediaDevices
+    .getUserMedia(
+        { audio: false, 
+        video: { width: 1280, height: 720 }, 
+    })
+    .then(function (stream) {
+    userStream = stream;
+    divVideoChatLobby.style = "display:none";
+    userVideo.srcObject = stream;
+    userVideo.onloadedmetadata = function(e) {
+        userVideo.play();
+    };
+
+})
+    .catch(function(err){
+        alert("Counld't access user media");
+    }
+    );
+});
+socket.on("full",function(){
+    alert("Room is full, Can't join")
+});
+socket.on("ready",function(){});
+socket.on("candidate",function(){});
+socket.on("offer",function(){});
+socket.on("answer",function(){});
