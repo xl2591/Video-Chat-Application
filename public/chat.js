52f119960278f355d5c60155ccb0105ccc7fff82
Xiaojingliu <xl2591@york.ac.uk>
2021-03-10 10:16:58 +0000

    refresh

--- public/chat.js
+++ public/chat.js
@@ -8,153 +8,146 @@ let roomInput = document.getElementById("roomName");
 let roomName;
 let creator = false;
 let rtcPeerConnection;
-let userStream;
+let userStream; 
 
-// Contains the stun server URL we will be using.
-let iceServers = {
-  iceServers: [
-    { urls: "stun:stun.services.mozilla.com" },
-    { urls: "stun:stun.l.google.com:19302" },
-  ],
+let iceServers ={ iceServers:
+    [
+    {urls: "stun:stun.services.mozilla.com"},
+    {urls: "stun:stun.l.google.com:19302"},
+],
+
 };
 
-joinButton.addEventListener("click", function () {
-  if (roomInput.value == "") {
-    alert("Please enter a room name");
-  } else {
-    roomName = roomInput.value;
-    socket.emit("join", roomName);
-  }
+joinButton.addEventListener('click',function() {
+
+    if(roomInput.value == "") {
+        alert("Please enter a room name")
+    }
+    else{
+        roomName = roomInput.value;
+        socket.emit("join",roomName);
+    }
 });
 
-// Triggered when a room is succesfully created.
-
-socket.on("created", function () {
-  creator = true;
-
-  navigator.mediaDevices
-    .getUserMedia({
-      audio: true,
-      video: { width: 500, height: 500 },
+socket.on("created",function(){
+    creator = true;
+  
+    navigator.mediaDevices
+    .getUserMedia( 
+        { audio: true, 
+        video: { width: 500, height: 500 }, 
     })
     .then(function (stream) {
-      /* use the stream */
-      userStream = stream;
-      divVideoChatLobby.style = "display:none";
-      userVideo.srcObject = stream;
-      userVideo.onloadedmetadata = function (e) {
+    userStream = stream;
+    divVideoChatLobby.style = "display:none";
+    userVideo.srcObject = stream;
+    userVideo.onloadedmetadata = function(e) {
         userVideo.play();
-      };
-    })
-    .catch(function (err) {
-      /* handle the error */
-      alert("Couldn't Access User Media");
-    });
+    };
+    
+}) 
+    .catch(function(err){
+        alert("Counld't access user media");
+    }
+    );
+
+});
+socket.on("joined",function(){
+    creator = false;
+   
+    navigator.mediaDevices
+      .getUserMedia(
+        { audio: true, 
+        video: { width: 500, height: 500 }, 
+       })
+       .then(function (stream) {
+        /* use the stream */
+        userStream = stream;
+        divVideoChatLobby.style = "display:none";
+        userVideo.srcObject = stream;
+        userVideo.onloadedmetadata = function (e) {
+          userVideo.play();
+        };
+        socket.emit("ready", roomName);
+      })
+    .catch(function(err){
+        alert("Counld't access user media");
+    }
+    );
 });
 
-// Triggered when a room is succesfully joined.
 
-socket.on("joined", function () {
-  creator = false;
-
-  navigator.mediaDevices
-    .getUserMedia({
-      audio: true,
-      video: { width: 500, height: 500 },
-    })
-    .then(function (stream) {
-      /* use the stream */
-      userStream = stream;
-      divVideoChatLobby.style = "display:none";
-      userVideo.srcObject = stream;
-      userVideo.onloadedmetadata = function (e) {
-        userVideo.play();
-      };
-      socket.emit("ready", roomName);
-    })
-    .catch(function (err) {
-      /* handle the error */
-      alert("Couldn't Access User Media");
-    });
+socket.on("full",function(){
+    alert("Room is full, Can't join")
 });
-
-// Triggered when a room is full (meaning has 2 people).
-
-socket.on("full", function () {
-  alert("Room is Full, Can't Join");
-});
-
-// Triggered when a peer has joined the room and ready to communicate.
 
 socket.on("ready", function () {
-  if (creator) {
-    rtcPeerConnection = new RTCPeerConnection(iceServers);
-    rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
-    rtcPeerConnection.ontrack = OnTrackFunction;
-    rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
-    rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
-    rtcPeerConnection
-      .createOffer()
-      .then((offer) => {
-        rtcPeerConnection.setLocalDescription(offer);
-        socket.emit("offer", offer, roomName);
-      })
-
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+    if (creator) {
+      rtcPeerConnection = new RTCPeerConnection(iceServers);
+      rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
+      rtcPeerConnection.ontrack = OnTrackFunction;
+      rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
+      rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
+      rtcPeerConnection
+        .createOffer()
+        .then((offer) => {
+          rtcPeerConnection.setLocalDescription(offer);
+          socket.emit("offer", offer, roomName);
+        })
+  
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  });
+//Triggerd on receiving an ice candidate from the peer
+socket.on("candidate",function(candidate){
+    let icecandidate = new RTCIceCandidate(candidate);
+    rtcPeerConnection.addIceCandidate(icecandidate);
 });
 
-// Triggered on receiving an ice candidate from the peer.
-
-socket.on("candidate", function (candidate) {
-  let icecandidate = new RTCIceCandidate(candidate);
-  rtcPeerConnection.addIceCandidate(icecandidate);
-});
-
-// Triggered on receiving an offer from the person who created the room.
-
+//Triggerd on receiving an offer from the person who created the room
 socket.on("offer", function (offer) {
-  if (!creator) {
-    rtcPeerConnection = new RTCPeerConnection(iceServers);
-    rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
-    rtcPeerConnection.ontrack = OnTrackFunction;
-    rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
-    rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
-    rtcPeerConnection.setRemoteDescription(offer);
-    rtcPeerConnection
-      .createAnswer()
-      .then((answer) => {
-        rtcPeerConnection.setLocalDescription(answer);
-        socket.emit("answer", answer, roomName);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-});
+    if (!creator) {
+      rtcPeerConnection = new RTCPeerConnection(iceServers);
+      rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
+      rtcPeerConnection.ontrack = OnTrackFunction;
+      rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
+      rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
+      rtcPeerConnection.setRemoteDescription(offer);
+      rtcPeerConnection
+        .createAnswer()
+        .then((answer) => {
+          rtcPeerConnection.setLocalDescription(answer);
+          socket.emit("answer", answer, roomName);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  });
 
 // Triggered on receiving an answer from the person who joined the room.
 
 socket.on("answer", function (answer) {
-  rtcPeerConnection.setRemoteDescription(answer);
-});
-
-// Implementing the OnIceCandidateFunction which is part of the RTCPeerConnection Interface.
-
-function OnIceCandidateFunction(event) {
-  console.log("Candidate");
-  if (event.candidate) {
-    socket.emit("candidate", event.candidate, roomName);
+    rtcPeerConnection.setRemoteDescription(answer);
+  });
+  
+  // Implementing the OnIceCandidateFunction which is part of the RTCPeerConnection Interface.
+  
+  function OnIceCandidateFunction(event) {
+    console.log("Candidate");
+    if (event.candidate) {
+      socket.emit("candidate", event.candidate, roomName);
+    }
   }
-}
-
-// Implementing the OnTrackFunction which is part of the RTCPeerConnection Interface.
-
-function OnTrackFunction(event) {
-  peerVideo.srcObject = event.streams[0];
-  peerVideo.onloadedmetadata = function (e) {
-    peerVideo.play();
-  };
-}
+  
+  // Implementing the OnTrackFunction which is part of the RTCPeerConnection Interface.
+  
+  function OnTrackFunction(event) {
+    peerVideo.srcObject = event.streams[0];
+    peerVideo.onloadedmetadata = function (e) {
+      peerVideo.play();
+    };
+  }
+  
